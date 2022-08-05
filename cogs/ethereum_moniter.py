56b800a6fcf14bf5cebc493f2def70083289f966
JoeyBrar <51928278+JoeyBrar@users.noreply.github.com>
2022-08-04 22:33:49 -0400

    fix?
    this conflicts with something and the bot isnt running. this is temporary fix

--- cogs/ethereum_moniter.py
+++ cogs/ethereum_moniter.py
@@ -10,54 +10,54 @@ import matplotlib.pyplot as plt
 
 headers = {'User-Agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
 
-class eth_button(discord.ui.View):
-  # class for the buttons with time frames for the graph of prices, put the buttons into the embed by 
-  # adding the "view = eth_button(timeout = optional_timeout_time)" field to the ctx.channel.send"
-  # gembed is an unadded function that creates the embed that corresponds to the button pressed
+# class eth_button(discord.ui.View):
+#   # class for the buttons with time frames for the graph of prices, put the buttons into the embed by 
+#   # adding the "view = eth_button(timeout = optional_timeout_time)" field to the ctx.channel.send"
+#   # gembed is an unadded function that creates the embed that corresponds to the button pressed
   
    
-    async def on_timeout(self):
-        for child in self.children:
-            child.disabled = True
-        await self.message.edit(view = self)
+#     async def on_timeout(self):
+#         for child in self.children:
+#             child.disabled = True
+#         await self.message.edit(view = self)
 
-    @discord.ui.button(label = "1 day", style = discord.ButtonStyle.gray)
-    async def button_press1(self, button, interaction):
-        await interaction.response.edit_message(embed=gembed(1))
+#     @discord.ui.button(label = "1 day", style = discord.ButtonStyle.gray)
+#     async def button_press1(self, button, interaction):
+#         await interaction.response.edit_message(embed=gembed(1))
 
-    @discord.ui.button(label = "1 week", style = discord.ButtonStyle.gray)
-    async def button_press2(self, button, interaction):
-        await interaction.response.edit_message(embed = gembed(2))
+#     @discord.ui.button(label = "1 week", style = discord.ButtonStyle.gray)
+#     async def button_press2(self, button, interaction):
+#         await interaction.response.edit_message(embed = gembed(2))
 
-    @discord.ui.button(label="2 weeks", style=discord.ButtonStyle.gray)
-    async def button_press3(self, button, interaction):
-        await interaction.response.edit_message(embed=gembed(3))
+#     @discord.ui.button(label="2 weeks", style=discord.ButtonStyle.gray)
+#     async def button_press3(self, button, interaction):
+#         await interaction.response.edit_message(embed=gembed(3))
 
-    @discord.ui.button(label="1 month", style=discord.ButtonStyle.gray)
-    async def button_press4(self, button, interaction):
-        await interaction.response.edit_message(embed=gembed(4))
+#     @discord.ui.button(label="1 month", style=discord.ButtonStyle.gray)
+#     async def button_press4(self, button, interaction):
+#         await interaction.response.edit_message(embed=gembed(4))
 
-    @discord.ui.button(label="3 months", style=discord.ButtonStyle.gray)
-    async def button_press5(self, button, interaction):
-        await interaction.response.edit_message(embed=gembed(5))
+#     @discord.ui.button(label="3 months", style=discord.ButtonStyle.gray)
+#     async def button_press5(self, button, interaction):
+#         await interaction.response.edit_message(embed=gembed(5))
 
-    @discord.ui.button(label="6 months", style=discord.ButtonStyle.gray)
-    async def button_press6(self, button, interaction):
-        await interaction.response.edit_message(embed=gembed(6))
+#     @discord.ui.button(label="6 months", style=discord.ButtonStyle.gray)
+#     async def button_press6(self, button, interaction):
+#         await interaction.response.edit_message(embed=gembed(6))
 
-    @discord.ui.button(label="1 year", style=discord.ButtonStyle.gray)
-    async def button_press7(self, button, interaction):
-        await interaction.response.edit_message(embed=gembed(7))
+#     @discord.ui.button(label="1 year", style=discord.ButtonStyle.gray)
+#     async def button_press7(self, button, interaction):
+#         await interaction.response.edit_message(embed=gembed(7))
 
-    @discord.ui.button(label="All time", style=discord.ButtonStyle.gray)
-    async def button_press8(self, button, interaction):
-        await interaction.response.edit_message(embed=gembed(8))
+#     @discord.ui.button(label="All time", style=discord.ButtonStyle.gray)
+#     async def button_press8(self, button, interaction):
+#         await interaction.response.edit_message(embed=gembed(8))
 
-    @discord.ui.button(label = "Kill embed", style=discord.ButtonStyle.danger)
-    async def button_press9(self, button, interaction):
-        for child in self.children:
-            child.disabled = True
-        await interaction.response.edit_message(embed=gembed(9), view = self)
+#     @discord.ui.button(label = "Kill embed", style=discord.ButtonStyle.danger)
+#     async def button_press9(self, button, interaction):
+#         for child in self.children:
+#             child.disabled = True
+#         await interaction.response.edit_message(embed=gembed(9), view = self)
 
 def timeStamp():
   tz = pytz.timezone('US/Eastern')
