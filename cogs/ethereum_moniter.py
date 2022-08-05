951d14072d4843c28a5f85ad629db458d7af3c1f
JoeyBrar <jorabrar2007@gmail.com>
2022-08-04 21:02:34 -0400

    update requirements.txt

--- cogs/ethereum_moniter.py
+++ cogs/ethereum_moniter.py
@@ -5,6 +5,7 @@ import pytz
 import requests
 from bs4 import BeautifulSoup
 import pandas
+from discord_ui import UI
 import matplotlib.pyplot as plt
 
 headers = {'User-Agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
