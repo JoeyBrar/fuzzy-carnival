374dfe63ab2e6b0c67ecb57d06dfd9a2eb34fc9e
JoeyBrar <51928278+JoeyBrar@users.noreply.github.com>
2022-08-14 16:18:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,16 @@ async def on_ready():
     print('Bot is online.\n')
     yagChannel = client.get_channel(931685534051479652)
     await yagChannel.send('<@608395567994765315> read the game name')
-    await client.change_presence(activity=discord.Game('Read the commit history it\'s not that hard'))
+    await client.change_presence(type=discord.ActivityType.playing,
+            large_image = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F001%2F535%2F091%2Fd97.jpg&imgrefurl=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fno-i-dont-think-i-will&tbnid=H3nFSiOx6SoRqM&vet=12ahUKEwj9pLbZksf5AhWammoFHSo5BGMQMygAegQIARAj..i&docid=66kj0AgwQSyAZM&w=680&h=382&q=no%20no%20i%20don%27t%20think%20i%20will%20image%20link&ved=2ahUKEwj9pLbZksf5AhWammoFHSo5BGMQMygAegQIARAj",
+            large_text = "no, no i don\'t think i will",
+            large_image = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F001%2F535%2F091%2Fd97.jpg&imgrefurl=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fno-i-dont-think-i-will&tbnid=H3nFSiOx6SoRqM&vet=12ahUKEwj9pLbZksf5AhWammoFHSo5BGMQMygAegQIARAj..i&docid=66kj0AgwQSyAZM&w=680&h=382&q=no%20no%20i%20don%27t%20think%20i%20will%20image%20link&ved=2ahUKEwj9pLbZksf5AhWammoFHSo5BGMQMygAegQIARAj",
+            large_text = "no, no i don\'t think i will",
+            small_image = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F001%2F535%2F091%2Fd97.jpg&imgrefurl=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fno-i-dont-think-i-will&tbnid=H3nFSiOx6SoRqM&vet=12ahUKEwj9pLbZksf5AhWammoFHSo5BGMQMygAegQIARAj..i&docid=66kj0AgwQSyAZM&w=680&h=382&q=no%20no%20i%20don%27t%20think%20i%20will%20image%20link&ved=2ahUKEwj9pLbZksf5AhWammoFHSo5BGMQMygAegQIARAj",
+            small_text = "no, no i don\'t think i will",
+            name = "no, no i don\'t think i will",
+            details = "no, no i don\'t think i will",
+            state = "no, no i don\'t think i will")))
 
   # useless stuff--------------------------------------------------------------------------
 @client.command()
